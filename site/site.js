655c900ec11b0afcfe5855023cf39281a8810b7d
kojem2012 <153429375+kojem2012@users.noreply.github.com>
2023-12-10 12:49:48 +0100

    Add files via upload

---
+++ site/site.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    const contentType = getContentType(filePath) || 'text/html';
+
+    fs.readFile(filePath, 'utf8', (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('File not found');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content);
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+function getContentType(filePath) {
+    const extname = path.extname(filePath);
+    switch (extname) {
+        case '.js':
+            return 'text/javascript';
+        case '.css':
+            return 'text/css';
+        case '.json':
+            return 'application/json';
+        case '.png':
+            return 'image/png';
+        case '.jpg':
+            return 'image/jpg';
+        case '.wav':
+            return 'audio/wav';
+        default:
+            return 'text/html';
+    }
+}
